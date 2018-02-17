d722c62ef98ec0bc3a4f3ec7b1a02c6edbc7d679
Saurabh <saurabhshrivastava956@gmail.com>
2018-02-17 16:50:03 +0530

    Created RESTApis

--- python-full-stack/firstFlaskApp/app.py
+++ python-full-stack/firstFlaskApp/app.py
@@ -6,4 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return "Welcome Bros!!!"
+
+@app.ruote('/store',methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>'):
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item',methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def create_item_in_store(name):
+    pass
 app.run()
