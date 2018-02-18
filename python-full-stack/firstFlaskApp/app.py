e6505dec01069f1e4711a5dc9872ea3d9e07af23
Saurabh <saurabhshrivastava956@gmail.com>
2018-02-18 19:31:54 +0530

    Create first restful App

--- python-full-stack/firstFlaskApp/app.py
+++ python-full-stack/firstFlaskApp/app.py
@@ -1,29 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+
+stores = [
+    {
+    'name': 'My wonderful storte',
+    'items':[
+            {
+                'name':'myItem',
+                'price': 35.99
+            }
+        ]
+    }
+]
 
 @app.route('/')
 def welcome():
     return "Welcome Bros!!!"
 
-@app.ruote('/store',methods=['POST'])
+@app.route('/store',methods=['POST'])
 def create_store():
     pass
 
-@app.route('/store/<string:name>'):
+@app.route('/store/<string:name>')
 def get_store(name):
     pass
 
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify("{'stores': stores}")
 
 @app.route('/store/<string:name>/item',methods=['POST'])
 def create_item_in_store(name):
     pass
 
 @app.route('/store/<string:name>/item')
-def create_item_in_store(name):
+def get_items_in_store(name):
     pass
 app.run()
